9e70a80d9793e740d8a4fba287e594888064fd3f
Fabrice LATRI <fabrice.latri@carbon-it.com>
2023-07-30 18:38:44 +0200

    linter

--- src/infrastructure/views/screens/auth/SignIn/validator/index.ts
+++ src/infrastructure/views/screens/auth/SignIn/validator/index.ts
@@ -1,14 +1,7 @@
 import * as yup from 'yup';
 
 export const signInValidatorSchema = yup.object({
-  email: yup
-    .string()
-    .email("L'email est invalide")
-    .lowercase()
-    .trim()
+  email: yup.string().email("L'email est invalide").lowercase().trim()
     .required("L'email est obligatoire"),
-  password: yup
-    .string()
-    .min(4, 'Minimum 4 caractères')
-    .required('Le mot de passe est obligatoire'),
+  password: yup.string().min(4, 'Minimum 4 caractères').required('Le mot de passe est obligatoire'),
 });
--- src/infrastructure/views/screens/auth/SignUp/validator/index.ts
+++ src/infrastructure/views/screens/auth/SignUp/validator/index.ts
@@ -1,16 +1,9 @@
 import * as yup from 'yup';
 
 export const signUpValidatorSchema = yup.object({
-  email: yup
-    .string()
-    .email("L'email est invalide")
-    .lowercase()
-    .trim()
+  email: yup.string().email("L'email est invalide").lowercase().trim()
     .required("L'email est obligatoire"),
-  password: yup
-    .string()
-    .min(4, 'Minimum 4 caractères')
-    .required('Le mot de passe est obligatoire'),
+  password: yup.string().min(4, 'Minimum 4 caractères').required('Le mot de passe est obligatoire'),
   confirmPassword: yup
     .string()
     .required('Le mot de passe est obligatoire')
