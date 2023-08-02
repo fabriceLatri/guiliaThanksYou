f97c9f171450ecc9734adb7e47e49fcdc1da8477
Fabrice LATRI <fabrice.latri@carbon-it.com>
2023-08-03 00:14:06 +0200

    Added image Component to Home Screen

--- src/infrastructure/views/screens/home/index.tsx
+++ src/infrastructure/views/screens/home/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Button, Center, Text } from 'native-base';
+import {
+  Button, Center, Image, Text,
+} from 'native-base';
 import { useHome } from '@infrastructure/views/screens/home/hooks/useHome';
+import { base64Uri } from '@infrastructure/views/screens/home/constants';
 
 export function Home() {
   const { signOut } = useHome();
 
   return (
     <Center style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Image
+        resizeMode="contain"
+        source={{
+          uri: base64Uri,
+        }}
+        style={{ flex: 1 }}
+        alt="Gigi in cosy"
+        size="2xl"
+      />
       <Text testID="home-screen-text">Home Screen</Text>
       <Button onPress={signOut}>Log Out</Button>
     </Center>
